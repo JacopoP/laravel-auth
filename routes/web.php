cd812317d6ff785db1dbf089c44394c11cbd0680
Jac22 <jacopo.prudentino@gmail.com>
2023-02-09 16:02:39 +0100

    Fatta migration della tabella Projects

--- routes/web.php
+++ routes/web.php
@@ -4,27 +4,22 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [MainController::class, 'home'])
 ->name('home');
 
 Route::middleware(['auth', 'verified'])
-   ->name('private.')
+   ->name('admin.')
    ->prefix('private')
    ->group(function () {
          Route::get('/', [MainController :: class, 'logged'])
          ->name('private');
    });
+
+
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
